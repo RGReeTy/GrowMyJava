1f1f09afeaecc03f3b6b29baa26922677001ff91
RGReeTy <be.a.reety@gmail.com>
2019-08-03 22:24:39 +0300

    added answers on 1-3 tasks

---
+++ src/com/GrowMyJava/BasicsOfSoftwareCodeDevelopment/Procedur_prog/Proced_progr/Task_2.java
@@ -0,0 +1,25 @@
+package com.GrowMyJava.BasicsOfSoftwareCodeDevelopment.Procedur_prog.Proced_progr;
+
+import java.util.Scanner;
+//Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+//𝑐 + √𝑐 2 + 4𝑏𝑑
+//2𝑏
+//− 𝑏 3 𝑑 + 𝑐 −2
+public class Task_2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number a= ");
+        float a = sc.nextFloat();
+        System.out.println("Input a number b= ");
+        float b = sc.nextFloat();
+        System.out.println("Input a number c= ");
+        float c = sc.nextFloat();
+        System.out.println("Result: " + funct(a, b, c));
+    }
+
+    public static float funct(float a, float b, float c) {
+        float z = ((b + (float) Math.sqrt((b * b + 4 * a * c))) / (2 * a)) - ((float) Math.pow(a, 3) * c + (float) Math.pow(b, -2));
+        return z;
+    }
+}
---
+++ src/com/GrowMyJava/BasicsOfSoftwareCodeDevelopment/Procedur_prog/Proced_progr/Task_3.java
@@ -0,0 +1,24 @@
+package com.GrowMyJava.BasicsOfSoftwareCodeDevelopment.Procedur_prog.Proced_progr;
+
+import java.util.Scanner;
+// Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+//𝑡𝑖𝑜𝑦 + 𝑑𝑝𝑡𝑧
+//𝑑𝑝𝑡𝑦 − 𝑡𝑖𝑜𝑧
+//∗ 𝑢𝑔 𝑦𝑧
+public class Task_3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number x = ");
+        double x = sc.nextDouble();
+        System.out.println("Input a number y = ");
+        double y = sc.nextDouble();
+        System.out.println("Result: " + funct(x, y));
+        sc.close();
+    }
+
+    public static double funct(double x, double y) {
+        double z = ((Math.sin(x) + Math.cos(y))/(Math.cos(x)-Math.sin(y)))*(Math.tan(x*y));
+        return z;
+    }
+}
