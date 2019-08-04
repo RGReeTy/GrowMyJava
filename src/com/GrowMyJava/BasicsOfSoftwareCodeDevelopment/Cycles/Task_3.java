665f0e48f75990ff0b986a29b120f9ac1a2eb5cc
RGReeTy <be.a.reety@gmail.com>
2019-08-04 23:02:55 +0300

    added answers on 4-6 tasks of first part of Module 1

--- src/com/GrowMyJava/BasicsOfSoftwareCodeDevelopment/Cycles/Task_3.java
+++ src/com/GrowMyJava/BasicsOfSoftwareCodeDevelopment/Cycles/Task_3.java
@@ -4,7 +4,7 @@ public class Task_3 {
     //Найти сумму квадратов первых ста чисел.
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i <= 100; i++) {
             sum += (int) Math.pow(i, 2);
             //System.out.println(sum);
         }
