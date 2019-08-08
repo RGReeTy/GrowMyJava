0a2a4f6f8f47f0df9312d89432f47246a7be07dd
RGReeTy <be.a.reety@gmail.com>
2019-08-08 22:41:33 +0300

    finished Module 1

--- src/com/GrowMyJava/BasicsOfSoftwareCodeDevelopment/Cycles/Task_6.java
+++ src/com/GrowMyJava/BasicsOfSoftwareCodeDevelopment/Cycles/Task_6.java
@@ -1,26 +1,12 @@
 package com.GrowMyJava.BasicsOfSoftwareCodeDevelopment.Cycles;
 
-import java.util.Scanner;
-
 public class Task_6 {
     //Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input a positive integer = ");
-        long x = sc.nextLong();
-        if (x < 0) {
-            System.out.println("Write a positive number!");
-        } else if (x == 1) {
-            System.out.println("Yours sum is 1 =|");
-        } else if (x == 0) {
-            System.out.println("Yours number is 0 =(");
-        } else {
-            long sum = 0;
-            for (int i = 1; i <= x; i++) {
-                sum += i;
-            }
-            System.out.println("Sum of numbers from 1 to " + x + " is " + sum);
-
+        //В Java тип char хранит 16-разрядное значение без знака в диапазоне от 0 до 65536
+        for (int i = 0; i < Character.MAX_VALUE; i++) {
+            System.out.println(i + " = " + (char) i);
         }
     }
 }
+
---
+++ src/com/GrowMyJava/BasicsOfSoftwareCodeDevelopment/Cycles/Task_8.java
@@ -0,0 +1,41 @@
+package com.GrowMyJava.BasicsOfSoftwareCodeDevelopment.Cycles;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    //Даны два числа. Определить цифры, входящие в запись как первого так и второго числа
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write first number:");
+        int firstNumber = sc.nextInt();
+        System.out.println("Write second number:");
+        int secondNumber = sc.nextInt();
+        compareDigits(firstNumber, secondNumber);
+    }
+
+    private static void compareDigits(int x, int y) {
+        int tempY = y;   //  temp param to enumeration
+        long uniqNums = 0;  //  new number of uniq digits
+        long uniqTemp;  //  temp param to enumeration
+        boolean hasUniNum = false;
+        while (x != 0) {         // compare x with temp y (tempY)
+            while (tempY != 0) {
+                if (x % 10 == tempY % 10) {  // есть одинаковое число из а в b
+                    uniqTemp = uniqNums;
+                    while (uniqTemp != 0) { // try find only unique digits
+                        if (uniqTemp % 10 == x % 10) hasUniNum = true;
+                        uniqTemp /= 10;
+                    }
+                    if (!hasUniNum) {
+                        uniqNums = uniqNums * 10 + (x % 10); // creating new unique number
+                    }
+                }
+                tempY /= 10; // decrease
+                hasUniNum = false;
+            }
+            x /= 10;
+            tempY = y; // return y to tempY
+        }
+        System.out.println("Digits included in first and second number is :" + uniqNums);
+    }
+}
