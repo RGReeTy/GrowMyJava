6bf4dfd5316cabf6bf1b81f749ddcddd77bf1e57
RGReeTy <be.a.reety@gmail.com>
2019-09-13 21:26:20 +0300

    added 12th task of Module 3 "Decomposition"

--- src/com/GrowMyJava/Algorithmization/Decomposition/Task_13.java
+++ src/com/GrowMyJava/Algorithmization/Decomposition/Task_13.java
@@ -1,11 +1,9 @@
 package com.GrowMyJava.Algorithmization.Decomposition;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-public class Task_12 {
+public class Task_13 {
     //12. Даны натуральные числа К и N. Написать метод(методы) формирования массива А, элементами которого
     //являются числа, сумма цифр которых равна К и которые не большее N.
 
