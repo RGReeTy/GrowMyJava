1171baded7e3f9bcc13f15b3a0a59ed6a0408b96
RGReeTy <be.a.reety@gmail.com>
2019-09-15 22:05:44 +0300

    added 12th and 13th task of Module 3 "Decomposition"

--- src/com/GrowMyJava/Algorithmization/Decomposition/Task_12.java
+++ src/com/GrowMyJava/Algorithmization/Decomposition/Task_12.java
@@ -8,7 +8,66 @@ import java.io.InputStreamReader;
 public class Task_12 {
     //12. Даны натуральные числа К и N. Написать метод(методы) формирования массива А, элементами которого
     //являются числа, сумма цифр которых равна К и которые не большее N.
-
     public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a number K..");
+        int K = Integer.parseInt(read.readLine());
+        System.out.println("Enter a number N..");
+        int N = Integer.parseInt(read.readLine());
+        if (K <= 0 || N <= 0) {
+            System.out.println("Input a positive numbers > 0!!");
+        } else {
+            int[] array = getRightArray(K, N);
+            if (array.length < 1) {
+                System.out.println("Can't to find any coincidences!");
+            } else {
+                for (int value : array) {
+                    System.out.print(value + " ");
+                }
+            }
+        }
+    }
+
+    private static int[] getRightArray(int sumOfDigits, int maxNumber) {
+        int count = 0;
+        int start = 0;
+        int summ;
+        int[] array = new int[maxNumber];
+        for (int i = 0; i < array.length; i++) {
+            //verify numbers from 0 to N
+            for (int j = start + 1; j < maxNumber; j++) {
+                summ = 0;
+                int temp = j;
+                while (temp > 0) {
+                    int digits = temp % 10;
+                    temp /= 10;
+                    summ += digits;
+                }
+                if (summ == sumOfDigits) {
+                    start = j;
+                    break;
+                }
+            }
+
+            if (i > 0) {
+                if (start == array[i - 1]) {
+                    start = 0;
+                    break;
+                }
+            }
+            array[i] = start;
+        }
+
+        // find final length of array
+        for (int value : array) {
+            if (value > 0) {
+                count++;
+            }
+        }
+
+        int[] arrayFinal = new int[count];
+        System.arraycopy(array, 0, arrayFinal, 0, arrayFinal.length);
+        return arrayFinal;
     }
 }
+
--- src/com/GrowMyJava/Algorithmization/Decomposition/Task_13.java
+++ src/com/GrowMyJava/Algorithmization/Decomposition/Task_13.java
@@ -1,12 +1,31 @@
 package com.GrowMyJava.Algorithmization.Decomposition;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task_13 {
-    //12. Даны натуральные числа К и N. Написать метод(методы) формирования массива А, элементами которого
-    //являются числа, сумма цифр которых равна К и которые не большее N.
+    //13. Два простых числа называются «близнецами», если они отличаются друг от друга на 2 (например, 41 и 43).
+    //Найти и напечатать все пары «близнецов» из отрезка [n,2n], где n - заданное натуральное число больше 2. Для
+    //решения задачи использовать декомпозицию.
 
     public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a number N..");
+        int N = Integer.parseInt(read.readLine());
+        if (N <= 2) {
+            System.out.println("Input a positive numbers > 2!!");
+        } else {
+            getTwins(N);
+        }
+    }
+
+    private static void getTwins(int n) {
+        for (int i = n; i < n * 2; i++) {
+            if (i < n * 2 - 1) {
+                System.out.println("( " + i + " and " + (i + 2) + ")");
+            }
+        }
     }
 }
