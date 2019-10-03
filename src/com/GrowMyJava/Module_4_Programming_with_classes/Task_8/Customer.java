232f0fbefdbeedcba01ed9fff3c8ae056c1d2c4e
RGReeTy <be.a.reety@gmail.com>
2019-10-03 17:53:45 +0300

    added part of 8th task of Module 4 "Programming with classes"

---
+++ src/com/GrowMyJava/Module_4_Programming_with_classes/Task_8/Customer.java
@@ -0,0 +1,105 @@
+package com.GrowMyJava.Module_4_Programming_with_classes.Task_8;
+
+import java.math.BigInteger;
+
+public class Customer {
+    //8. Создать класс Customer, спецификация которого приведена ниже. Определить конструкторы, set- и get- методы
+    //и метод toString(). Создать второй класс, агрегирующий массив типа Customer, с подходящими конструкторами
+    //и методами. Задать критерии выбора данных и вывести эти данные на консоль.
+    //Класс Customer: id, фамилия, имя, отчество, адрес, номер кредитной карточки, номер банковского счета.
+    //Найти и вывести:
+    //a) список покупателей в алфавитном порядке;
+    //b) список покупателей, у которых номер кредитной карточки находится в заданном интервале
+
+    private int customerID;
+    private String name;
+    private String surname;
+    private String secondName;
+    private String address;
+    private long cardNumber;
+
+    public Customer(int customerID, String name, String secondName, String surname, String address, long cardNumber) {
+        this.customerID = customerID;
+        this.name = name;
+        this.surname = surname;
+        this.secondName = secondName;
+        this.address = address;
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer {" +
+                "customerID=" + customerID +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", address='" + address + '\'' +
+                ", cardNumber=" + cardNumber +
+                '}';
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null) {
+            this.name = name;
+        } else {
+            System.out.println("Wrong name");
+        }
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        if (name != null) {
+            this.surname = surname;
+        } else {
+            System.out.println("Wrong name");
+        }
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        if (address != null) {
+            this.address = address;
+        } else {
+            System.out.println("Wrong address");
+        }
+    }
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(long cardNumber) {
+        if (customerID > 999_999_999 & customerID < 1_000_000_000) {
+            this.cardNumber = cardNumber;
+        } else {
+            System.out.println("Wrong number of credit card");
+        }
+    }
+}
---
+++ src/com/GrowMyJava/Module_4_Programming_with_classes/Task_8/Task_8.java
@@ -0,0 +1,26 @@
+package com.GrowMyJava.Module_4_Programming_with_classes.Task_8;
+
+import java.util.ArrayList;
+
+public class Task_8 {
+    //8. Создать класс Customer, спецификация которого приведена ниже. Определить конструкторы, set- и get- методы
+    //и метод toString(). Создать второй класс, агрегирующий массив типа Customer, с подходящими конструкторами
+    //и методами. Задать критерии выбора данных и вывести эти данные на консоль.
+    //Класс Customer: id, фамилия, имя, отчество, адрес, номер кредитной карточки, номер банковского счета.
+    //Найти и вывести:
+    //a) список покупателей в алфавитном порядке;
+    //b) список покупателей, у которых номер кредитной карточки находится в заданном интервале
+
+    public static void main(String[] args) {
+        Customer[] customers = {
+                new Customer(101, "Ivan", "Olegovich", "Rogov", "Minsk", 9254_1646_4584_1455L),
+                new Customer(111, "Elena", "Ivanovna", "Pears", "Brest", 9254_1646_4464_9999L),
+                new Customer(133, "Chan", "Lee", "Kim", "Huanhe", 1123_1314_4455_7799L),
+        };
+
+        for (Customer customer : customers) {
+            System.out.println(customer.toString());
+        }
+
+    }
+}
