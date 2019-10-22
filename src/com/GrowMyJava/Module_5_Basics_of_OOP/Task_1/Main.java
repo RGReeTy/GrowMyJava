de129e89c2e1e3eb7d646ee51c1ba1e916f86c3c
RGReeTy <be.a.reety@gmail.com>
2019-10-22 21:02:42 +0300

    added 1th task of Module 5 "Basics of OOP"

---
+++ src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/Main.java
@@ -0,0 +1,10 @@
+package com.GrowMyJava.Module_5_Basics_of_OOP.Task_1;
+
+public class Main {
+    //1. Создать объект класса Текстовый файл, используя классы Файл, Директория. Методы: создать, переименовать,
+    //вывести на консоль содержимое, дополнить, удалить.
+
+    public static void main(String[] args) {
+
+    }
+}
