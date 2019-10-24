ccf39a9a9949cc7d2a9108d8ef8ad1d5eafe3761
RGReeTy <be.a.reety@gmail.com>
2019-10-24 21:31:32 +0300

    added part of 1st task of Module 5

--- src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/File.java
+++ src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/File.java
@@ -1,4 +1,25 @@
 package com.GrowMyJava.Module_5_Basics_of_OOP.Task_1.source;
 
 public class File {
+
+    private String nameOfFile;
+
+    public File(String nameOfFile) {
+        this.nameOfFile = nameOfFile;
+    }
+
+    public String getNameOfFile() {
+        return nameOfFile;
+    }
+
+    public void setNameOfFile(String nameOfFile) {
+        this.nameOfFile = nameOfFile;
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "nameOfFile='" + nameOfFile + '\'' +
+                '}';
+    }
 }
---
+++ src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/TextFile.java
@@ -0,0 +1,9 @@
+package com.GrowMyJava.Module_5_Basics_of_OOP.Task_1.source;
+
+public class TextFile extends File {
+
+
+    public TextFile(String nameOfFile) {
+        super(nameOfFile);
+    }
+}
