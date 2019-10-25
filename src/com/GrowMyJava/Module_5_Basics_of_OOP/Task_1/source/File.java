14498dc46e33a8947db9bdd4703bcba85a1315b6
RGReeTy <be.a.reety@gmail.com>
2019-10-25 21:36:30 +0300

    added part of 1st task of Module 5

--- src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/File.java
+++ src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/File.java
@@ -1,11 +1,23 @@
 package com.GrowMyJava.Module_5_Basics_of_OOP.Task_1.source;
 
+import java.time.LocalDateTime;
+
 public class File {
 
     private String nameOfFile;
+    private boolean isReadable = false;
+    private boolean isWritable = false;
+    private int size;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private LocalDateTime opened;
 
     public File(String nameOfFile) {
         this.nameOfFile = nameOfFile;
+        this.isReadable = true;
+        this.isWritable = true;
+        this.size = 0;
+        this.created = LocalDateTime.now();
     }
 
     public String getNameOfFile() {
@@ -16,10 +28,51 @@ public class File {
         this.nameOfFile = nameOfFile;
     }
 
-    @Override
-    public String toString() {
-        return "File{" +
-                "nameOfFile='" + nameOfFile + '\'' +
-                '}';
+    public boolean isReadable() {
+        return isReadable;
+    }
+
+    public void setReadable(boolean readable) {
+        isReadable = readable;
+    }
+
+    public boolean isWritable() {
+        return isWritable;
+    }
+
+    public void setWritable(boolean writable) {
+        isWritable = writable;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    public LocalDateTime getOpened() {
+        return opened;
+    }
+
+    public void setOpened(LocalDateTime opened) {
+        this.opened = opened;
     }
 }
--- src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/TextFile.java
+++ src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/TextFile.java
@@ -3,7 +3,4 @@ package com.GrowMyJava.Module_5_Basics_of_OOP.Task_1.source;
 public class TextFile extends File {
 
 
-    public TextFile(String nameOfFile) {
-        super(nameOfFile);
-    }
 }
