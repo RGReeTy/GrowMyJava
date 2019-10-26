1744001c851b95145c12db248e049d26f848b0a4
RGReeTy <be.a.reety@gmail.com>
2019-10-26 23:25:12 +0300

    added part of 1st task of Module 5

--- src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/Directory.java
+++ src/com/GrowMyJava/Module_5_Basics_of_OOP/Task_1/source/Directory.java
@@ -1,4 +1,9 @@
 package com.GrowMyJava.Module_5_Basics_of_OOP.Task_1.source;
 
+import java.util.List;
+
 public class Directory {
+
+    private String nameOfDirectory;
+    private List<File> fileList;
 }
