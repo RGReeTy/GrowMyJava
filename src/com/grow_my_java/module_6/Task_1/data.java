bc83859c74c33326409123abd2e2cb61a156c729
RGReeTy <be.a.reety@gmail.com>
2019-11-04 21:16:12 +0300

    Added part of 1th task of Module 6

---
+++ src/com/grow_my_java/module_6/Task_1/data.java
@@ -0,0 +1,4 @@
+package com.grow_my_java.module_6.Task_1;
+
+public class data {
+}
---
+++ src/com/grow_my_java/module_6/Task_1/source/book/Type.java
@@ -0,0 +1,6 @@
+package com.grow_my_java.module_6.Task_1.source.book;
+
+public enum Type {
+    PAPER,
+    E_BOOK
+}
