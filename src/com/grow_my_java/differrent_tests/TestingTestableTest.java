e965491a21b143d2ce6245d6442b543151e891fc
RGReeTy <be.a.reety@gmail.com>
2019-12-22 15:18:58 +0300

    fixed some code

--- src/com/grow_my_java/differrent_tests/TestingTestableTest.java
+++ src/com/grow_my_java/differrent_tests/TestingTestableTest.java
@@ -1,7 +1,7 @@
 package com.grow_my_java.differrent_tests;
 
 
-public class TestingTestobleTest {
+public class TestingTestableTest {
     public static void main(String[] args) {
         System.out.println(args);
     }
