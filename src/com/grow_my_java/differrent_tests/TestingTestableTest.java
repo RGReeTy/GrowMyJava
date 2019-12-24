c7c36315f3e4aac536663f940d402728b5fbe109
RGReeTy <be.a.reety@gmail.com>
2019-12-24 21:03:14 +0300

    fixed some code

--- src/com/grow_my_java/differrent_tests/TestingTestableTest.java
+++ src/com/grow_my_java/differrent_tests/TestingTestableTest.java
@@ -3,6 +3,8 @@ package com.grow_my_java.differrent_tests;
 
 public class TestingTestableTest {
     public static void main(String[] args) {
+
         System.out.println(args);
+        System.out.println(args.hashCode());
     }
 }
