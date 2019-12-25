05a37350b8c2ce10ec6ae1500387304a445d5a83
RGReeTy <be.a.reety@gmail.com>
2019-12-25 20:20:08 +0300

    added some code

--- src/com/grow_my_java/differrent_tests/TestingTestableTest.java
+++ src/com/grow_my_java/differrent_tests/TestingTestableTest.java
@@ -1,10 +1,26 @@
 package com.grow_my_java.differrent_tests;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class TestingTestableTest {
     public static void main(String[] args) {
 
-        System.out.println(args);
-        System.out.println(args.hashCode());
+        List<String> list = new ArrayList<String>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+        list.add("Six");
+        list.add("Seven");
+        list.add("Eight");
+        list.add("Nine");
+        list.add("Ten");
+        Stream stream = list.stream();
+
+        stream.filter(x -> x.toString().length() == 3).forEach(System.out::println);
     }
 }
