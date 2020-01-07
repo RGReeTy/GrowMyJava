d3d128c58d482b612aa6d30e7bed5b57888b17e2
RGReeTy <be.a.reety@gmail.com>
2020-01-07 22:14:20 +0300

    added some code

---
+++ src/com/grow_my_java/differrent_tests/GetEvenNumberFollowingOfScan.java
@@ -0,0 +1,25 @@
+package com.grow_my_java.differrent_tests;
+
+import java.util.Scanner;
+
+public class GetEvenNumberFollowingOfScan {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        System.out.println(getNumber(number));
+
+    }
+
+    private static int getNumber(int number) {
+        if (number == 0) return 2;
+        if (number % 2 == 0) {
+            return number + 2;
+        } else {
+            return number + 1;
+        }
+    }
+}
