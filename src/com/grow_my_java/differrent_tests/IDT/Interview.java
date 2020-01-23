0ff4f63cb74f9a5cb76e3f7b7440c60ae316d284
RGReeTy <be.a.reety@gmail.com>
2020-01-23 18:21:59 +0300

    added some code

--- src/com/grow_my_java/differrent_tests/IDT/Interview.java
+++ src/com/grow_my_java/differrent_tests/IDT/Interview.java
@@ -17,10 +17,30 @@ public class Interview {
 
         //version 2 (starting from 2, columns at two lines)
         for (int i = 1; i <= 9; i++) {
-            for (int j = 2; j <= 9; j++) {
+            for (int j = 2; j <= 5; j++) {
                 System.out.print(j + " x " + i + " = " + i * j + "\t");
             }
             System.out.println();
+        }
+        System.out.println();
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 6; j <= 9; j++) {
+                System.out.print(j + " x " + i + " = " + i * j + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("----------------------------------------------------------------");
+
+        //version 3 with using 'while' loop (starting from 2, columns at one line)
+        int x = 1, y = 2;
+        while (x != 10) {
+            while (y != 10) {
+                System.out.print(y + " x " + x + " = " + y * x + "\t");
+                y++;
+            }
+            System.out.println();
+            x++;
+            y = 2;
         }
     }
 }
