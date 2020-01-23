5891baddbd993b3e6de54001536e23552fbded7d
RGReeTy <be.a.reety@gmail.com>
2020-01-23 17:47:41 +0300

    added some code

--- src/com/grow_my_java/differrent_tests/IDT/Interview.java
+++ src/com/grow_my_java/differrent_tests/IDT/Interview.java
@@ -2,23 +2,25 @@ package com.grow_my_java.differrent_tests.IDT;
 
 //17.01.2020 14:00
 
-import java.util.stream.IntStream;
-
 public class Interview {
 
     public static void main(String[] args) {
 
+        //version 1 (starting from 1, columns at one line)
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
-                System.out.print(j + " * " + i + " = " + i * j);
-                System.out.print("\t");
+                System.out.print(j + " x " + i + " = " + i * j + "\t");
             }
             System.out.println();
         }
+        System.out.println("----------------------------------------------------------------");
 
-
-        IntStream.of(10, 11, 12).filter(x -> x < 12).map(x -> x * 10).forEach(System.out::println);
+        //version 2 (starting from 2, columns at two lines)
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 2; j <= 9; j++) {
+                System.out.print(j + " x " + i + " = " + i * j + "\t");
+            }
+            System.out.println();
+        }
     }
-
-
 }
