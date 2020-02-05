72d17a9b21f0b276e7f5b23dfffe9c877bbc05b4
RGReeTy <be.a.reety@gmail.com>
2020-02-05 15:54:52 +0300

    added some code

---
+++ src/com/grow_my_java/differrent_tests/other/InterfaceTest.java
@@ -0,0 +1,16 @@
+package com.grow_my_java.differrent_tests.other;
+
+import java.io.IOException;
+
+interface InterfaceTest {
+    public static void me2() {
+
+    }
+
+    abstract void me1();
+
+    default void me3() {
+
+    }
+
+}
---
+++ src/com/grow_my_java/differrent_tests/other/StringEqualses.java
@@ -0,0 +1,17 @@
+package com.grow_my_java.differrent_tests.other;
+
+public class StringEqualses {
+
+    public static void main(String[] args) {
+        System.out.println("A ");
+        synchronized (args) {
+            System.out.println("B ");
+            try {
+                args.wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        System.out.println("C ");
+    }
+}
