d12443739ad4ece1278065681dead4713ead5d10
RGReeTy <be.a.reety@gmail.com>
2020-02-06 20:42:56 +0300

    added some code

---
+++ src/com/grow_my_java/differrent_tests/other/CurrentThreadDemo.java
@@ -0,0 +1,22 @@
+package com.grow_my_java.differrent_tests.other;
+
+public class CurrentThreadDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = Thread.currentThread();
+        System.out.println("Текущий поток: " + t);
+        t.setName("My Thread");
+        System.out.println("После изменения имени: " + t);
+        try {
+            for (int n = 5; n > 0; n--) {
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток завершен");
+        }
+        t.join(5);
+        System.out.println("t.isInterrupted() = " + t.isInterrupted());
+        t.interrupt();
+        System.out.println("t.isInterrupted() = " + t.isInterrupted());
+    }
+}
