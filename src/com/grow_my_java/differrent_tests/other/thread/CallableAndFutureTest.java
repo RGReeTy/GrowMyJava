e047ce9b41ecedf94b50cf385f23c8e701f02175
RGReeTy <be.a.reety@gmail.com>
2020-02-10 21:15:49 +0300

    added some code

--- src/com/grow_my_java/differrent_tests/other/thread/CallableAndFutureTest.java
+++ src/com/grow_my_java/differrent_tests/other/thread/CallableAndFutureTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.*;
 public class CallableAndFutureTest {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
+
         Future<Integer> future = executorService.submit(() -> {
             System.out.println("Starting");
             try {
