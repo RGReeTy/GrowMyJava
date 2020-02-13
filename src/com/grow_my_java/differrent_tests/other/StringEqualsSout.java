e3babe221d51355f37860742774ec869421dab66
RGReeTy <be.a.reety@gmail.com>
2020-02-13 22:20:38 +0300

    added string test code

---
+++ src/com/grow_my_java/differrent_tests/other/StringEqualsSout.java
@@ -0,0 +1,12 @@
+package com.grow_my_java.differrent_tests.other;
+
+public class StringEqualsSout {
+    public static void main(String[] args) {
+        String str1 = "java";
+        String str2 = "java";
+        String str3 = new String("java");
+
+        System.out.println(str1 == str2 && str1 == str3);
+
+    }
+}
