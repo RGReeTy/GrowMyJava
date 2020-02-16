d4aba9b6f75fd3ca9a195cb882954d9af64b8517
RGReeTy <be.a.reety@gmail.com>
2020-02-16 23:10:46 +0300

    added string test code

--- src/com/grow_my_java/differrent_tests/other/StringEqualsSout.java
+++ src/com/grow_my_java/differrent_tests/other/StringEqualsSout.java
@@ -2,6 +2,7 @@ package com.grow_my_java.differrent_tests.other;
 
 public class StringEqualsSout {
     public static void main(String[] args) {
+
         String str1 = "java";
         String str2 = "java";
         String str3 = new String("java");
