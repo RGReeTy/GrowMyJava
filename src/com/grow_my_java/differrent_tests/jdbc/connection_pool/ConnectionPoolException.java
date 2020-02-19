8a98edc6b76252f63a8fa2db4a08160fe754fb27
RGReeTy <be.a.reety@gmail.com>
2020-02-19 21:00:26 +0300

    added connection pool

---
+++ src/com/grow_my_java/differrent_tests/jdbc/connection_pool/ConnectionPoolException.java
@@ -0,0 +1,10 @@
+package com.grow_my_java.differrent_tests.jdbc.connection_pool;
+
+public class ConnectionPoolException extends Exception {
+
+    private static final long serialVersionUID = 2088396460993072884L;
+
+    public ConnectionPoolException(String message, Exception e) {
+        super(message, e);
+    }
+}
