a8dce149e87792fa5cbf86d20377e774e721766a
RGReeTy <be.a.reety@gmail.com>
2020-03-03 21:59:25 +0300

    added ..

--- src/com/grow_my_java/differrent_test/fun/A.java
+++ src/com/grow_my_java/differrent_test/fun/A.java
@@ -10,8 +10,10 @@ public class A {
     }
 
     public static void main(String[] args) {
+
         short a1 = 1;
         float b1 = 2;
+
         System.out.print(m(a1) + " : " + m(b1));
     }
 }
