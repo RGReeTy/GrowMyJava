993ed0348f09e300a5fc08297c094bc321de5ba5
RGReeTy <be.a.reety@gmail.com>
2020-03-08 23:28:02 +0300

    added ..

--- src/com/grow_my_java/differrent_test/other/CollatzConjecture.java
+++ src/com/grow_my_java/differrent_test/other/CollatzConjecture.java
@@ -13,6 +13,7 @@ Sequence of integers in a single line, separated by spaces.
 Sample Input 1:17
 Sample Output 1:17 52 26 13 40 20 10 5 16 8 4 2 1
      */
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
