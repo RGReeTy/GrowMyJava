8dff1f42b0ede299c7054113347b7f4440edeeae
RGReeTy <be.a.reety@gmail.com>
2020-09-21 19:49:34 +0300

    add some code

---
+++ src/com/grow_my_java/differrent_test/innowise/Test.java
@@ -0,0 +1,8 @@
+package com.grow_my_java.differrent_test.innowise;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
--- src/com/grow_my_java/differrent_test/other/equalsIntegerValues.java
+++ src/com/grow_my_java/differrent_test/other/equalsIntegerValues.java
@@ -1,21 +1,21 @@
 package com.grow_my_java.differrent_test.other;
 
 public class equalsIntegerValues {
-    public static void main(String[] args) {
-        Integer int1 = new Integer(145);
-        Integer int2 = new Integer(45);
-        Number int3 = new Integer(145);
-        Integer int4 = 145;
-        Integer int5 = 145;
-        Integer int6 = 45;
-        Integer int7 = 45;
-        int x = 45;
-        System.out.println(int1 > int2);
-        System.out.println(x < int1);
-        //System.out.println(x < int3);
-        System.out.println(int1 == int4);
-        System.out.println(int4 == int5);
-        System.out.println(int6 == int7);
-        System.out.println(int2 == x);
-    }
+//    public static void main(String[] args) {
+//        Integer int1 = new Integer(145);
+//        Integer int2 = new Integer(45);
+//        Number int3 = new Integer(145);
+//        Integer int4 = 145;
+//        Integer int5 = 145;
+//        Integer int6 = 45;
+//        Integer int7 = 45;
+//        int x = 45;
+//        System.out.println(int1 > int2);
+//        System.out.println(x < int1);
+//        //System.out.println(x < int3);
+//        System.out.println(int1 == int4);
+//        System.out.println(int4 == int5);
+//        System.out.println(int6 == int7);
+//        System.out.println(int2 == x);
+//    }
 }
